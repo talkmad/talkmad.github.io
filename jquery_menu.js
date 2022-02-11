bb9b7a6aea28d054628a909352c3861045e23c88
talkmad <99480403+talkmad@users.noreply.github.com>
2022-02-11 12:58:19 +0100

    Newly added files

---
+++ jquery_menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".menu").click(function(){
+        $(".dropdown").toggle();
+    })
+    
+})
+
+$(document).ready(function(){
+    $(".container" ).click(function(){
+        $(".dropdown").hide();
+    })
+})
+
+$(document).ready(function(){
+    $(".logo" ).click(function(){
+        $(".dropdown").hide();
+    })
+})
